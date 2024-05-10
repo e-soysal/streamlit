05921b678ee15d2412f28f5db4de2d6e44fdbc8c
e-soysal <emilie.rosenlund@gmail.com>
2024-05-10 21:20:48 +0300

    Update streamlit_app_test.py

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -6,8 +6,8 @@ Created on 10 05 2024
 import random
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 
-st.title("This is survival prob simulation 1")
 
 def ruin_prob(f, w_0):
     """Ruin probability"""
@@ -19,14 +19,18 @@ def sigmoid(x,a,b):
     return 1 / (1 + np.exp(-x*a+b))
 
 def main():
+    st.title("Benchmark process: $\mu$: 275, $\sigma$: 265, $w_0$: 17500")
     # User inputs
-    mu = st.slider('Drift:', value=0.95, min_value=0.00, max_value=1.00, step=0.01)
-    sigma = st.slider('Volatility:', value=0.05, min_value=0.00, max_value=1.00, step=0.01)
-    
+    f = 275/265**2
+    w_0 = 17500
+    a = st.slider('Drift:', value=0.95, min_value=0.00, max_value=1.00, step=0.01)
+    b = st.slider('Volatility:', value=0.05, min_value=0.00, max_value=1.00, step=0.01)
+    x = np.linspace(0, 6000, 100)  # Generate 100 points between -10 and 10
+    g = sigmoid(x,a,b)
+    P = ruin_prob(g*f,w_0)
     if st.button('Simulate'):
-        prob = ruin_prob(mu/sigma**2, 100)
         # Display results
-        st.write(prob)
+        st.write(P)
       
 if __name__ == '__main__':
     main()
