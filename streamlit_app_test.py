2d140647eb272972d397c9bf9ddb2bad0eabfd28
e-soysal <emilie.rosenlund@gmail.com>
2024-05-10 20:33:07 +0300

    Update streamlit_app_test.py

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -5,8 +5,6 @@ Created on 10 05 2024
 """
 import random
 import streamlit as st
-from streamlit import session_state
-import matplotlib.pyplot as plt
 import numpy as np
 
 st.title("This is survival prob simulation 1")
