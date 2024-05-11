5703419977668fa377e889f49e55eb4d70036d2f
e-soysal <emilie.rosenlund@gmail.com>
2024-05-11 13:53:00 +0300

    Update streamlit_app_test.py

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -7,7 +7,8 @@ import random
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pandas as pd
+from scipy.stats import linregress
 
 def ruin_prob(f, w_0):
     """Ruin probability"""
@@ -19,25 +20,42 @@ def sigmoid(x,a,b):
     return 1 / (1 + np.exp(-(x-a)*b))
 
 def main():
-    st.write("Benchmark process: $\mu$: 275, $\sigma$: 265, $w_0$: 17500")
+    st.write("Benchmark process: $\mu$: 280, $\sigma$: 286, $w_0$: 17500")
     st.write("To stay below 2 degrees, we need to invest 5 percent of GDP")
-# User inputs
+    # User inputs
     w_0 = 17500
-    mu = st.slider('Drift, $\mu$:', value=275, min_value=0, max_value=500, step=5)
-    sigma = st.slider('Volatility, $\sigma$:', value=265, min_value=0, max_value=3000, step=5)
-    x = np.linspace(0, 1000, 100) 
+    
+    # Display results   
+    hist_data = { 'year': [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
+                 'GDP': [9968.671891, 10192.94482, 10440.52934,	10553.45026, 10776.47927, 11138.26003, 11246.50192, 11398.57076, 11668.42089, 12105.37947, 12495.37837, 12969.54552, 13464.8027, 13658.73942, 13391.3059, 13899.49329, 14263.1084, 14535.86611, 14820.27166, 15139.44768, 15461.1147, 15777.59079, 
+                         16186.46995, 16588.58729, 16877.47189, 16213.37544, 17091.35075, 17527.18851]}
+    hist_data = pd.DataFrame(hist_data)
+    
+    growth = hist_data['GDP'].diff()
+    mu_bm = np.mean(growth)
+    sigma_bm = np.std(growth)
+    mu = st.slider('Drift, $\mu$:', value=int(np.round(mu_bm,0)), min_value=0, max_value=500, step=1)
+    sigma = st.slider('Volatility, $\sigma$:', value=int(np.round(sigma_bm,0)), min_value=0, max_value=3000, step=1)
     f = mu/sigma**2
     P = ruin_prob(f,w_0)
-    st.write('Ruin probability', P)
     
-    # Display results
+    x = np.arange(1,100)+2022
+    rd = pd.DataFrame(np.random.normal(mu, sigma, len(x)))
     
+    random_path = 17527.18851 +  rd.cumsum()
+    
+    # Fit linear regression
+    slope, intercept, r_value, p_value, std_err = linregress(hist_data['year'], hist_data['GDP'])
+    trend_GDP = slope * hist_data['year'] + intercept
+    print(slope)
+    print(std_err)
     fig, ax = plt.subplots(1,2, figsize=(10, 4))
-    ax[0].plot(x, g, label = "Impact factor", color = 'black')
-    ax[0].set_xlabel('Mitigation expenditure, X')
-    ax[0].set_ylabel('g(X)')
+    ax[0].plot(hist_data.year, hist_data.GDP, label = "Historical", color = 'black')
+    ax[0].plot(x, random_path, label = "Random_path", color = 'red')
+    ax[0].set_xlabel('Year')
+    ax[0].set_ylabel('GDP per capita [PPP, 2017 USD]')
     ax[0].legend()
-    ax[0].set_title('Fig. 2A: Odds function')
+    ax[0].set_title('GDP')
     ax[0].grid(True)
 
     # Plotting the ruin probability
