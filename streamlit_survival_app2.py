dee67905c6b003c6f9f76c008d346edd0181ab47
e-soysal <emilie.rosenlund@gmail.com>
2024-05-11 18:14:28 +0300

    Update streamlit_survival_app2.py

--- streamlit_survival_app2.py
+++ streamlit_survival_app2.py
@@ -62,7 +62,7 @@ def main():
     ax[0].scatter(x[-1], transition_GDP[-1], label = "", color = 'Black')
     ax[0].annotate('w_0', xy=(x[-1], transition_GDP[-1]), xytext=(x[-1]-5, transition_GDP[-1]-1000),
              arrowprops=dict(facecolor='black', arrowstyle='->'))
-    ax[0].set_xlim(0,np.max(transition_GDP,hist_data.GDP))
+    ax[0].set_xlim(0,np.max([transition_GDP,hist_data.GDP]))
     ax[0].set_xlabel('Year')
     ax[0].set_title('GDP per capita [PPP, 2017 USD]')
     ax[0].legend()
@@ -85,6 +85,7 @@ def main():
              arrowprops=dict(facecolor='black', arrowstyle='->'))
     ax[1].legend()
     ax[1].set_title('Scaling factor of odds function compared to benchmark')
+    ax[1].set_xlim(0,1)
     
     # Plotting the ruin probability
     st.pyplot(plt)
