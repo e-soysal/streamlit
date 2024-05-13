e46ea437ccc88c02abb3301099c70af585a510b8
e-soysal <emilie.rosenlund@gmail.com>
2024-05-13 16:47:47 +0300

    Update streamlit_survival_app3.py

--- streamlit_survival_app3.py
+++ streamlit_survival_app3.py
@@ -69,59 +69,72 @@ def main():
     
     st.write('Choose the shape of the drift mu(x) by adjusting shape and location:')
     a = st.slider('Impact point (left to right)', value=1100, min_value=0, max_value=800, step=1)
-    b = st.slider('Shape (flat to steep):', value=0.0001, min_value=-0.0010, max_value=0.0010, step=0.0001)
+    b_input = st.slider('Shape (flat to steep):', value=1, min_value=0, max_value=100, step=1)
     st.write('Choose a the yearly expenditure on mitigation in USD per capita per year:')
     mitigation = st.slider(':orange-background[Mitigation expenditure in USD per year:]', value=mitigation_year, min_value=0, max_value=1000, step=10, label_visibility = "collapsed" )    
+
+    b = b_input/1000
+    
     
     # Find transition GDP until 2032
     x = np.arange(0,11)+2022
     transition_growth = (mu_bm-mitigation)*np.ones(len(x))
     transition_growth[0] = 0
     transition_GDP = w_0 + transition_growth.cumsum()
-
-    # Plot wealth projections
-    fig, ax = plt.subplots(1,2, figsize=(10, 4))
-    ax[0].plot(hist_data.year, hist_data.GDP, label = "Historical", color = 'black')
-    ax[0].plot(x, transition_GDP, label = "Transition path", color = 'red')
-    ax[0].scatter(x[-1], transition_GDP[-1], label = "", color = 'Black')
-    ax[0].annotate('w_0', xy=(x[-1], transition_GDP[-1]), xytext=(x[-1]-5, transition_GDP[-1]-1000),
-             arrowprops=dict(facecolor='black', arrowstyle='->'))
-    ax[0].set_ylim(0,np.max([np.max(transition_GDP),np.max(hist_data.GDP[:])])+1000)
-    ax[0].set_xlabel('Year')
-    ax[0].set_title('GDP per capita [PPP, 2017 USD]')
-    ax[0].legend()
-    ax[0].grid(True)
-
+    
     # Calculate new ruin probability 2032
     mu_mitigation = mu_x(mitigation, a, b, mu_bm)
     P = ruin_prob(mu_mitigation/sigma_bm**2, transition_GDP[-1])
     st.write('Probability of ruin: ', P)
     
+    # Simulate path in future
+    x_2 = np.arange(0,90)+x[-1]
+    rd = np.random.normal(mu_mitigation, sigma_bm, len(x_2))
+    rd[0] = 0
+    random_path = transition_GDP[-1] +  rd.cumsum()
+    
+    # Plot wealth projections
+    fig, ax = plt.subplots(2,1, figsize=(6, 10))
+    ax[1].plot(hist_data.year, hist_data.GDP, label = "Historical", color = 'black')
+    ax[1].plot(x, transition_GDP, label = "Transition path", color = 'red')
+    ax[1].plot(x_2, random_path, label = "Simulated path", color = 'red')
+    ax[1].scatter(x[-1], transition_GDP[-1], label = "", color = 'Black')
+    ax[1].annotate('w_0', xy=(x[-1], transition_GDP[-1]), xytext=(x[-1]-5, transition_GDP[-1]-1000),
+             arrowprops=dict(facecolor='black', arrowstyle='->'))
+    ax[1].set_ylim(0,np.max([np.max(transition_GDP),np.max(hist_data.GDP[:]),np.max(random_path)])+1000)
+    ax[1].set_xlabel('Year')
+    ax[1].set_title('GDP per capita [PPP, 2017 USD]')
+    ax[1].legend()
+    ax[1].grid(True)
+
+    
     x_input = np.linspace(0, 1200, 100)
     mu_input = mu_x(x_input, a, b, mu_bm)
     
     # Find optimal mitigation
-    X_init = mitigation # Initial guess
+    X_init = mitigation_year # Initial guess
     
     # Create a partial function with fixed arguments
     partial_function = partial(prob_sig_function, a=a, b=b, w_0=w_0, mu_0=mu_bm, sigma_0=sigma_bm, years=len(x)-1)
     result = minimize(partial_function, X_init)
     if result.x[0]>0:
         st.write('Optimal spending on mitigation: ', np.round(result.x[0],0), 'Optimal probability of ruin: ', np.exp(result.fun))
-        ax[1].scatter(result.x[0], mu_x(result.x[0], a,b, mu_bm), label = "Optimal strategy", color = 'blue')
+        ax[0].scatter(result.x[0], mu_x(result.x[0], a,b, mu_bm), label = "Optimal strategy", color = 'blue')
     else:
         st.write('Optimal spending on mitigation:        Optimal ruin probability: ')
         
-    ax[1].scatter(mitigation, mu_x(mitigation,a,b, mu_bm), label = "Chosen strategy", color = 'Black')
-    ax[1].fill_between([mitigation_year-50, mitigation_year+50], [mu_bm,mu_bm],[0,0] ,color = 'green', label = '2 degrees required spending', alpha=.3)
-    ax[1].plot([125, 125], [0,mu_bm], color = 'black', linestyle = "--", label = 'Current spending')
-    ax[1].plot(x_input, mu_input, color = 'black')
+    ax[0].scatter(mitigation, mu_x(mitigation,a,b, mu_bm), label = "Chosen strategy", color = 'Black')
+    ax[0].fill_between([mitigation_year-50, mitigation_year+50], [mu_bm+50,mu_bm+50],[0,0] ,color = 'green', label = '2 degrees required spending', alpha=.3)
+    ax[0].fill_between([125-5, 125+5], [mu_bm+50,mu_bm+50],[0,0] ,color = 'red', label = 'Current spending', alpha=.3)
+    ax[0].plot([a, a], [mu_bm-25,mu_bm+25], color = 'black', linestyle = "--", label = '')
+    ax[0].annotate('a', xy=(a, mu_bm-25))
+    ax[0].plot(x_input, mu_input, color = 'black')
     
-    ax[1].legend()
-    ax[1].set_title('Drift, mu(X)')
-    ax[1].set_xlabel('Mitigation in USD per year')
+    ax[0].legend()
+    ax[0].set_title('Drift, mu(X)')
+    ax[0].set_xlabel('Mitigation in USD per year')
+    ax[0].set_ylim(0,mu_bm+50)
      
-    # Plotting the ruin probability
     st.pyplot(plt)
     plt.show()
 
